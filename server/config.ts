6d107f918410dfd363404dbe30819300ade90702
magnardd <magnardd@stud.ntnu.no>
2021-10-29 20:25:08 +0200

    Corrected the environment variables in the config files

---
+++ server/config.ts
@@ -0,0 +1,4 @@
+process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
+process.env.MYSQL_USER = 'solveol_gameshed';
+process.env.MYSQL_PASSWORD = 'gameshed';
+process.env.MYSQL_DATABASE = 'solveol_gameshed';
