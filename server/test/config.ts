db9d66924a456cb71dd7722358bcd41a62ce6e8b
steieg <steieg@stud.ntnu.no>
2021-10-31 19:12:23 +0100

    Lagt til IGDB bruker og secret i config.ts

--- server/test/config.ts
+++ server/test/config.ts
@@ -2,3 +2,6 @@ process.env.MYSQL_HOST = 'mysql.stud.ntnu.no';
 process.env.MYSQL_USER = 'solveol_gameshed';
 process.env.MYSQL_PASSWORD = 'gameshed';
 process.env.MYSQL_DATABASE = 'solveol_test';
+
+process.env.IGDB_CLIENT_ID = 'jggwkki694bcpccn7nwz0d5uoi1dn4'
+process.env.IGDB_CLIENT_SECRET = 'pysekvcpf8zhkqdzjyrzygy7a3wpv5'
