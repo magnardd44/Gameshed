12ea1cc5cc02aba35a97101a606e3485197b0482
steieg <steieg@stud.ntnu.no>
2021-11-21 20:02:10 +0100

    Test home-components

--- client/test/home-components.test.tsx
+++ client/test/home-components.test.tsx
@@ -5,7 +5,7 @@ import MockAdapter from 'axios-mock-adapter';
 import { shallow } from 'enzyme';
 import { history, Home, ReviewHome } from '../src/home-components';
 
-//import { gameService } from '../src/services/game-service';
+import userService from '../src/services/user-service';
 import { ColumnCentre, Form, Card, Alert, Button, Container, Column } from '../src/widgets';
 
 describe('Home component', () => {
@@ -14,6 +14,54 @@ describe('Home component', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('Button search game', () => {
+    const wrapper = shallow(<Home />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(0).simulate('click');
+
+    expect(spy).toBeCalledWith('/search');
+  });
+
+  test('Button search review', () => {
+    const wrapper = shallow(<Home />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(1).simulate('click');
+
+    expect(spy).toBeCalledWith('/reviews');
+  });
+
+  test('Button add game', () => {
+    const wrapper = shallow(<Home />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Legg til spill' }).simulate('click');
+
+    expect(spy).toBeCalledWith('/addGame');
+  });
+
+  test('No register button on login', () => {
+    userService.token = { id: 1, token: '' };
+    const wrapper = shallow(<Home />);
+
+    expect(wrapper.find({ children: 'Registrer deg' })).toHaveLength(0);
+  });
+
+  test('Button register user', () => {
+    userService.token = null;
+    const wrapper = shallow(<Home />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Registrer deg' }).simulate('click');
+
+    expect(spy).toBeCalledWith('/user');
+  });
 });
 
 describe('ReviewHome component', () => {
@@ -21,5 +69,55 @@ describe('ReviewHome component', () => {
     const wrapper = shallow(<ReviewHome />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('Button sort by genre', () => {
+    const wrapper = shallow(<ReviewHome />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(0).simulate('click');
+
+    expect(spy).toBeCalledWith('/reviews-by-genre');
+  });
+
+  test('Button sort by genre', () => {
+    const wrapper = shallow(<ReviewHome />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(1).simulate('click');
+
+    expect(spy).toBeCalledWith('/reviews-by-platform');
+  });
+
+  test('Button sort by stars', () => {
+    const wrapper = shallow(<ReviewHome />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(2).simulate('click');
+
+    expect(spy).toBeCalledWith('/reviews-by-stars');
+  });
+
+  test('Button sort by date', () => {
+    const wrapper = shallow(<ReviewHome />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Søk' }).at(3).simulate('click');
+
+    expect(spy).toBeCalledWith('/reviews-by-date');
+  });
+
+  test('Button all reviews', () => {
+    const wrapper = shallow(<ReviewHome />);
+
+    const spy = jest.spyOn(history, 'push');
+
+    wrapper.find({ children: 'Alle anmeldelser' }).simulate('click');
+
+    expect(spy).toBeCalledWith('/publishedReviews');
   });
 });
